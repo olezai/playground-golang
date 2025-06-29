978230611e070b0800fc87f2ec5d3e2ee1cf0522
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-06-29 23:13:06 +0200

    added error handling for empty string

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,13 +1,20 @@
 package greetings
 
 // this package will be used as a module
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// return a greeting that embeds the name in a message
+func Hello(name string) (string, error) {
+	// if no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// if a name was received return a greeting that embeds the name in a message
 	// the := operator is a shortcut for declaring and initializing a variable in one line
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 
-	return message
+	return message, nil
 }
--- hello/main.go
+++ hello/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"example.com/greetings"
 	"fmt"
+	"log"
+
+	"example.com/greetings"
 )
 
 func main() {
 	// get a greeting message and print it
-	message := greetings.Hello("Amadeus")
+	// set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// request a greeting message;
+	message, err := greetings.Hello("")
+	// if an error was returned, print it to the console
+	// and exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if no error was returned, print the returned message
+	// to the console
 	fmt.Println(message)
 }
