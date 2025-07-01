7f66bf70dcbd9b532d7fc4004d44ee0bb35eee71
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-07-01 15:27:21 +0200

    improvements with filepath and logging

--- zipdemo/main.go
+++ zipdemo/main.go
@@ -2,21 +2,37 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 	"zipdemo/zipdemo"
 )
 
 func main() {
-	f := "nicefile.txt"
-	a := "nicearchive.zip"
-	c := "This is some random text that will be zipped.\nHello World!\nThis is a demo file."
+	// Configure file paths
+	filename := "nicefile.txt"
+	archiveName := "nicearchive.zip"
+	content := `This is some random text that will be zipped.
+Hello World!
+This is a demo file.`
 
-	err := zipdemo.CreateFile(f, c)
-	if err != nil {
-		log.Fatal(err)
+	// Create working directory if it doesn't exist
+	workDir := "output"
+	if err := os.MkdirAll(workDir, 0755); err != nil {
+		log.Fatalf("Failed to create working directory: %v", err)
 	}
 
-	err = zipdemo.ZipFile(a, f)
-	if err != nil {
-		log.Fatal(err)
+	// Use filepath.Join for cross-platform path handling
+	filePath := filepath.Join(workDir, filename)
+	archivePath := filepath.Join(workDir, archiveName)
+
+	// Create and zip the file with error handling
+	if err := zipdemo.CreateFile(filePath, content); err != nil {
+		log.Fatalf("Failed to create file: %v", err)
 	}
+
+	if err := zipdemo.ZipFile(archivePath, filePath); err != nil {
+		log.Fatalf("Failed to create zip archive: %v", err)
+	}
+
+	log.Printf("Successfully created zip archive at: %s", archivePath)
 }
