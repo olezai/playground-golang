04cd42dc4dd431a1ad694c23764948caf8d615f8
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-07-01 14:44:39 +0200

    added slices and randomizer to quotes demo

--- externalmod/main.go
+++ externalmod/main.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 
-import "rsc.io/quote"
+import "rsc.io/quote" // module that
+
+import "math/rand"
 
 func main() {
-	fmt.Println(quote.Go())
+	quotes := []string{quote.Go(), quote.Hello(), quote.Glass(), quote.Opt()}
+	fmt.Println(quotes[rand.Intn(len(quotes))])
+	// fmt.Println(quote.Go())
 }
